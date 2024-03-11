352c5c16726630e6ce207cb8b5bdc98acf062c70
kate <extenuating.circumstancess@gmail.com>
2024-03-11 09:04:04 +0300

    no disign

--- project/app/src/main/java/com/example/myapplication/MainActivity2.kt
+++ project/app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -2,10 +2,22 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.view.View
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+    }
+
+    fun startTesting(view: View) {
+        val intent = Intent(this, MainActivity3::class.java)
+        startActivity(intent)
+    }
+
+    fun returnBack(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
